affca072b1250d817eaabbfa8c1b266b7cc77a38
John <jbartlettdesign@gmail.com>
2021-07-01 17:48:53 -0500

    pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,43 @@
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 const webpack = require("webpack");
-
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 module.exports = {
-entry: './assets/js/script.js',
+  entry:{
+  app: "./assets/js/script.js",
+  events: "./assets/js/event.js",
+  schedule: "./assets/js/schedule.js",
+  tickets: "./assets/js/tickets.js"
+},
 
 output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+  filename: "[name].bundle.js",
+    path: __dirname + "/dist",
+  },
+ 
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name (file) {
+                return "[path][name].[ext]"
+              },
+              publicPath: function(url) {
+                return url.replace("../", "/assets/")
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
   },
 
 plugins: [
@@ -18,6 +48,21 @@ plugins: [
       }),
       new BundleAnalyzerPlugin({
         analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+      }),
+      new WebpackPwaManifest({
+        name: "Food Event",
+        short_name: "Foodies",
+        description: "An app that allows you to view upcoming food events.",
+        start_url: "../index.html",
+        background_color: "#01579b",
+        theme_color: "#ffffff",
+        fingerprints: false,
+        inject: false,
+        icons: [{
+          src: path.resolve("assets/img/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons")
+        }]
       })
 ],
 
